d4c1c52a1e6e750fe8a3d1d30a3406feeea85286
zhuaiballl <ghc@mail.bnu.edu.cn>
2023-08-07 16:19:40 +0800

    fix bug: block() should add bytes rather than write

--- homohash.go
+++ homohash.go
@@ -23,139 +23,9 @@ const (
 	init7 = 0
 )
 
-const (
-	ind0  = 40
-	ind1  = 37
-	ind2  = 56
-	ind3  = 15
-	ind4  = 50
-	ind5  = 32
-	ind6  = 61
-	ind7  = 62
-	ind8  = 8
-	ind9  = 43
-	ind10 = 20
-	ind11 = 7
-	ind12 = 25
-	ind13 = 51
-	ind14 = 1
-	ind15 = 26
-	ind16 = 38
-	ind17 = 21
-	ind18 = 13
-	ind19 = 45
-	ind20 = 57
-	ind21 = 23
-	ind22 = 6
-	ind23 = 16
-	ind24 = 17
-	ind25 = 31
-	ind26 = 35
-	ind27 = 42
-	ind28 = 55
-	ind29 = 9
-	ind30 = 59
-	ind31 = 36
-	ind32 = 2
-	ind33 = 19
-	ind34 = 58
-	ind35 = 29
-	ind36 = 44
-	ind37 = 63
-	ind38 = 0
-	ind39 = 48
-	ind40 = 33
-	ind41 = 11
-	ind42 = 34
-	ind43 = 4
-	ind44 = 54
-	ind45 = 46
-	ind46 = 39
-	ind47 = 52
-	ind48 = 18
-	ind49 = 47
-	ind50 = 10
-	ind51 = 41
-	ind52 = 53
-	ind53 = 28
-	ind54 = 49
-	ind55 = 5
-	ind56 = 30
-	ind57 = 12
-	ind58 = 60
-	ind59 = 14
-	ind60 = 27
-	ind61 = 22
-	ind62 = 24
-	ind63 = 3
-)
+var ind = [64]uint8{40, 37, 56, 15, 50, 32, 61, 62, 8, 43, 20, 7, 25, 51, 1, 26, 38, 21, 13, 45, 57, 23, 6, 16, 17, 31, 35, 42, 55, 9, 59, 36, 2, 19, 58, 29, 44, 63, 0, 48, 33, 11, 34, 4, 54, 46, 39, 52, 18, 47, 10, 41, 53, 28, 49, 5, 30, 12, 60, 14, 27, 22, 24, 3}
 
-const (
-	p0  = 96
-	p1  = 213
-	p2  = 79
-	p3  = 9
-	p4  = 42
-	p5  = 123
-	p6  = 77
-	p7  = 11
-	p8  = 93
-	p9  = 116
-	p10 = 105
-	p11 = 29
-	p12 = 79
-	p13 = 67
-	p14 = 24
-	p15 = 71
-	p16 = 49
-	p17 = 153
-	p18 = 106
-	p19 = 45
-	p20 = 133
-	p21 = 180
-	p22 = 111
-	p23 = 231
-	p24 = 92
-	p25 = 27
-	p26 = 95
-	p27 = 222
-	p28 = 40
-	p29 = 85
-	p30 = 13
-	p31 = 125
-	p32 = 165
-	p33 = 176
-	p34 = 41
-	p35 = 206
-	p36 = 7
-	p37 = 96
-	p38 = 171
-	p39 = 113
-	p40 = 26
-	p41 = 134
-	p42 = 61
-	p43 = 213
-	p44 = 143
-	p45 = 127
-	p46 = 47
-	p47 = 180
-	p48 = 14
-	p49 = 152
-	p50 = 149
-	p51 = 204
-	p52 = 17
-	p53 = 177
-	p54 = 116
-	p55 = 195
-	p56 = 77
-	p57 = 248
-	p58 = 255
-	p59 = 72
-	p60 = 127
-	p61 = 213
-	p62 = 55
-	p63 = 129
-)
+var para = [64]uint8{0x60, 0xd5, 0x4f, 0x9, 0x2a, 0x7b, 0x4d, 0xb, 0x5d, 0x74, 0x69, 0x1d, 0x4f, 0x43, 0x18, 0x47, 0x31, 0x99, 0x6a, 0x2d, 0x85, 0xb4, 0x6f, 0xe7, 0x5c, 0x1b, 0x5f, 0xde, 0x28, 0x55, 0xd, 0x7d, 0xa5, 0xb0, 0x29, 0xce, 0x7, 0x60, 0xab, 0x71, 0x1a, 0x86, 0x3d, 0xd5, 0x8f, 0x7f, 0x2f, 0xb4, 0xe, 0x98, 0x95, 0xcc, 0x11, 0xb1, 0x74, 0xc3, 0x4d, 0xf8, 0xff, 0x48, 0x7f, 0xd5, 0x37, 0x81}
 
 type homo struct {
 	h   [8]uint32
@@ -263,14 +133,9 @@ func (h *homo) BlockSize() int {
 
 func (h *homo) Write(p []byte) (nn int, err error) {
 	if h.len == 0 {
-		h.h[0] = uint32(galMultiply(p[0], p0)) | uint32(galMultiply(p[0], p1))<<8 | uint32(galMultiply(p[0], p2))<<16 | uint32(galMultiply(p[0], p3))<<24
-		h.h[1] = uint32(galMultiply(p[0], p4)) | uint32(galMultiply(p[0], p5))<<8 | uint32(galMultiply(p[0], p6))<<16 | uint32(galMultiply(p[0], p7))<<24
-		h.h[2] = uint32(galMultiply(p[0], p8)) | uint32(galMultiply(p[0], p9))<<8 | uint32(galMultiply(p[0], p10))<<16 | uint32(galMultiply(p[0], p11))<<24
-		h.h[3] = uint32(galMultiply(p[0], p12)) | uint32(galMultiply(p[0], p13))<<8 | uint32(galMultiply(p[0], p14))<<16 | uint32(galMultiply(p[0], p15))<<24
-		h.h[4] = uint32(galMultiply(p[0], p16)) | uint32(galMultiply(p[0], p17))<<8 | uint32(galMultiply(p[0], p18))<<16 | uint32(galMultiply(p[0], p19))<<24
-		h.h[5] = uint32(galMultiply(p[0], p20)) | uint32(galMultiply(p[0], p21))<<8 | uint32(galMultiply(p[0], p22))<<16 | uint32(galMultiply(p[0], p23))<<24
-		h.h[6] = uint32(galMultiply(p[0], p24)) | uint32(galMultiply(p[0], p25))<<8 | uint32(galMultiply(p[0], p26))<<16 | uint32(galMultiply(p[0], p27))<<24
-		h.h[7] = uint32(galMultiply(p[0], p28)) | uint32(galMultiply(p[0], p29))<<8 | uint32(galMultiply(p[0], p30))<<16 | uint32(galMultiply(p[0], p31))<<24
+		for i := 0; i < 8; i++ {
+			h.h[i] = uint32(galMultiply(p[0], para[(i<<2)])) | uint32(galMultiply(p[0], para[(i<<2)|1]))<<8 | uint32(galMultiply(p[0], para[(i<<2)|2]))<<16 | uint32(galMultiply(p[0], para[(i<<2)|3]))<<24
+		}
 	}
 	nn = len(p)
 	h.len += uint64(nn)
@@ -305,11 +170,6 @@ func (h *homo) checkSum() [Size]byte {
 	len := h.len
 	// Padding. Add 0 bits until 56 bytes mod 64.
 	var tmp [64]byte
-	//if len%64 < 56 {
-	//	h.Write(tmp[:56-len%64])
-	//} else {
-	//	h.Write(tmp[:64+56-len%64])
-	//}
 	if len%64 != 0 {
 		h.Write(tmp[:64-len%64])
 	}
@@ -330,18 +190,21 @@ func (h *homo) checkSum() [Size]byte {
 
 // block updates h[] in h based on content of p[], which is a block of data
 func block(h *homo, p []byte) {
-	h0, h1, h2, h3, h4, h5, h6, h7 := h.h[0], h.h[1], h.h[2], h.h[3], h.h[4], h.h[5], h.h[6], h.h[7]
+	newh := make([]uint32, 8)
+	copy(newh, h.h[:])
+	var hest [Size]byte
+	for i := 0; i < 8; i++ {
+		binary.BigEndian.PutUint32(hest[i<<2:], newh[i])
+	}
 	for len(p) >= chunk {
-		h0 = uint32(galMultiply(p[ind0], p0)) | uint32(galMultiply(p[ind1], p1))<<8 | uint32(galMultiply(p[ind2], p2))<<16 | uint32(galMultiply(p[ind3], p3))<<24
-		h1 = uint32(galMultiply(p[ind4], p4)) | uint32(galMultiply(p[ind5], p5))<<8 | uint32(galMultiply(p[ind6], p6))<<16 | uint32(galMultiply(p[ind7], p7))<<24
-		h2 = uint32(galMultiply(p[ind8], p8)) | uint32(galMultiply(p[ind9], p9))<<8 | uint32(galMultiply(p[ind10], p10))<<16 | uint32(galMultiply(p[ind11], p11))<<24
-		h3 = uint32(galMultiply(p[ind12], p12)) | uint32(galMultiply(p[ind13], p13))<<8 | uint32(galMultiply(p[ind14], p14))<<16 | uint32(galMultiply(p[ind15], p15))<<24
-		h4 = uint32(galMultiply(p[ind16], p16)) | uint32(galMultiply(p[ind17], p17))<<8 | uint32(galMultiply(p[ind18], p18))<<16 | uint32(galMultiply(p[ind19], p19))<<24
-		h5 = uint32(galMultiply(p[ind20], p20)) | uint32(galMultiply(p[ind21], p21))<<8 | uint32(galMultiply(p[ind22], p22))<<16 | uint32(galMultiply(p[ind23], p23))<<24
-		h6 = uint32(galMultiply(p[ind24], p24)) | uint32(galMultiply(p[ind25], p25))<<8 | uint32(galMultiply(p[ind26], p26))<<16 | uint32(galMultiply(p[ind27], p27))<<24
-		h7 = uint32(galMultiply(p[ind28], p28)) | uint32(galMultiply(p[ind29], p29))<<8 | uint32(galMultiply(p[ind30], p30))<<16 | uint32(galMultiply(p[ind31], p31))<<24
+		for i := 0; i < Size; i++ {
+			hest[i] = galAdd(hest[i], galMultiply(p[ind[i]], para[i]))
+		}
+		for i := 0; i < 8; i++ {
+			newh[i] = uint32(hest[i<<2]) | uint32(hest[(i<<2)|1])<<8 | uint32(hest[(i<<2)|2])<<16 | uint32(hest[(i<<2)|3])<<24
+		}
 		p = p[chunk:]
 	}
-	h.h[0], h.h[1], h.h[2], h.h[3], h.h[4], h.h[5], h.h[6], h.h[7] = h0, h1, h2, h3, h4, h5, h6, h7
+	copy(h.h[:], newh)
 	return
 }
